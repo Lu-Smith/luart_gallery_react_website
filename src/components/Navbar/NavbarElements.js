cbf6f6787af7998ec48657a110ea0b1d2e2a10a3
Luna Smith <artistlunasmith@gmail.com>
2022-06-22 12:42:54 +0100

    Added Menu elements and MobileIcon
    Installed react-icons/fa
    Added bootstrap

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,13 +1,12 @@
 import styled from "styled-components";
 import { Link as LinkR } from "react-router-dom";
-// import { Link as LinkS } from `react-scroll`
+import { Link as LinkS } from "react-scroll";
 
 export const Nav = styled.nav`
   background: #000;
   height: 80px;
-  /* margin-top: -80px; */
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   font-size: 1rem;
   position: sticky;
@@ -21,8 +20,7 @@ export const Nav = styled.nav`
 
 export const NavbarContainer = styled.div`
 display: flex;
-justify-content: space-between;
-height:80px;
+height: 80px;
 z-index: 1;
 width: 100%:
 padding: 0 24px;
@@ -30,12 +28,59 @@ max-width: 1100px;
 `;
 
 export const NavLogo = styled(LinkR)`
+  display: flex;
   color: #fff;
-  justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
-  display: flex;
   align-items: center;
   margin-left: 24px;
+  font-weight: bold;
   text-decoration: none;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 786px) {
+    display: block;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    transform: translates(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #fff;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-itmes: center;
+  list-style: none;
+  text-align: center;
+  margin: 40px 200px;
+
+  @media screen and (max-width: 786px) {
+    display: none;
+  }
+`;
+
+export const NavItem = styled.li`
+  height: 80px;
+`;
+
+export const NavLinks = styled(LinkS)`
+color: #fff;
+display: flex;
+align-item: center;
+text-decoration: none;
+padding: 0 1 rem;
+height: 100%;
+cursor: pointer;
+
+&.active {
+    border-bottom: 3px solid #01bf71:
+    
+}
+
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
@@ -7,6 +16,14 @@ const Navbar = () => {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">LuArt Gallery</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about"> About </NavLinks>
+            </NavItem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
