e7cbcfe2c4426b72c5d923b8e729fcb3a803efa0
Luna Smith <artistlunasmith@gmail.com>
2022-06-22 20:15:11 +0100

    Added ButtonElement and text for Home

--- src/components/HomeSection/index.js
+++ src/components/HomeSection/index.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
-import { HomeContainer, HomeBg, VideoBg } from "./HomeElements";
+import { Button } from "../ButtonElement";
+import {
+  HomeContainer,
+  HomeBg,
+  VideoBg,
+  HomeContent,
+  HomeH1,
+  HomeP,
+  HomeBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HomeElements";
 
 const HomeSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
     <HomeContainer id="home">
       <HomeBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HomeBg>
+      <HomeContent>
+        <HomeH1>Welcome to Art World</HomeH1>
+        <HomeP>Open your mind and find yourself.</HomeP>
+        <HomeBtnWrapper>
+          <Button
+            to="contact"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get in touch {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HomeBtnWrapper>
+      </HomeContent>
     </HomeContainer>
   );
 };
